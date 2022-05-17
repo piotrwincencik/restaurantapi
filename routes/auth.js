276542d557ca7c74a223e4d89e530e04de3548f2
winencikpiotr <wincencikpiotr@gmail.com>
2022-05-17 12:07:35 +0200

    loginWithJWT

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
 
-//Register
+//REGISTER
 
 router.post("/register", async (req, res)=>{
     const newUser = new User({
@@ -21,6 +21,49 @@ router.post("/register", async (req, res)=>{
     }
    
 });
+
+//LOGIN
+router.post("/login", async(req,res)=>{
+    try{
+        const user = await User.findOne(
+            {
+                username: req.body.username
+            }
+        );
+
+        if(!user){
+            res.status(401).json("Wrong username.");
+        } 
+
+        const descryptedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.SECRET_PASSWORD
+        );
+
+        const password = descryptedPassword.toString(CryptoJS.enc.Utf8)
+
+        if(password!==req.body.password){
+            res.status(401).json("Wrong password.");
+        }
+        const jwtToken = jwt.sign({
+            id:user._id,
+            isAdmin: user.isAdmin,
+            
+        }, process.env.SECRET_JWT,
+        {expiresIn:"1d"}
+        );
+
+
+        const {newPassword, ...others}=user._doc;
+        res.status(200).json({...others, jwtToken});
+
+
+    }
+   
+    catch(error){
+        res.status(400).json(error)
+    }
+})
 
 
 
